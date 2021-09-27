089242b9eb7607b4184df24723b6dc55c8ad873f
Manolis Papadakis <manopapad@gmail.com>
2021-09-27 14:07:31 -0700

    Separate cudf from cuda installation

--- setup_conda.sh
+++ setup_conda.sh
@@ -26,23 +26,26 @@ if [[ $# -ge 1 && ( "$1" == "-h" || "$1" == "--help" ) ]]; then
     echo "Arguments read from the environment:"
     echo "  CONDA_ENV : name of conda environment to create (default: legate)"
     echo "  CONDA_ROOT : where to install conda (if not already installed)"
-    echo "  CUDA_VER : CUDA runtime version to install from conda (if installing"
-    echo "             Rapids) (default: match system version)"
+    echo "  CUDA_VER : CUDA runtime version to install from conda (if applicable)"
+    echo "             (default: match system version)"
     echo "  PYTHON_VER : Python version to use (default: 3.8)"
-    echo "  USE_RAPIDS : whether to install Rapids (required for running"
-    echo "               legate.pandas on GPUs) (default: 1)"
+    echo "  USE_CUDA : include CUDA support (default: auto-detected)"
+    echo "  USE_CUDF : install cuDF from Rapids (if including CUDA; cuDF is"
+    echo "             required for running legate.pandas on GPUs) (default: 1)"
     exit
 fi
 
 # Read arguments
 export CONDA_ENV="${CONDA_ENV:-legate}"
-if [[ -z "${CUDA_VER+x}" ]]; then
-    if command -v nvcc &> /dev/null; then
-        export CUDA_VER="$(nvcc --version | grep release | awk '{ print $5 }' | sed 's/.$//')"
-    fi
+export USE_CUDA="${USE_CUDA:-1}"
+if ! command -v nvcc &> /dev/null; then
+    export USE_CUDA=0
+fi
+if [[ "$USE_CUDA" == 1 && -z "${CUDA_VER+x}" ]]; then
+    export CUDA_VER="$(nvcc --version | grep release | awk '{ print $5 }' | sed 's/.$//')"
 fi
 export PYTHON_VER="${PYTHON_VER:-3.8}"
-export USE_RAPIDS="${USE_RAPIDS:-1}"
+export USE_CUDF="${USE_CUDF:-1}"
 
 # Install conda & load conda functions into this subshell
 if command -v conda &> /dev/null; then
@@ -67,10 +70,15 @@ if conda info --envs | grep -q "^$CONDA_ENV "; then
     echo "Error: Conda environment $CONDA_ENV already exists" 1>&2
     exit 1
 fi
-if [[ "$USE_RAPIDS" == 1 ]]; then
+if [[ "$USE_CUDA" == 1 && "$USE_CUDF" == 1 ]]; then
+     conda create --yes --name "$CONDA_ENV" \
+         -c rapidsai -c nvidia -c conda-forge -c defaults \
+         python="$PYTHON_VER" cudatoolkit="$CUDA_VER" cudf=0.19 \
+        "$@"
+elif [[ "$USE_CUDA" == 1 ]]; then
     conda create --yes --name "$CONDA_ENV" \
-        -c rapidsai -c nvidia -c conda-forge -c defaults \
-        python="$PYTHON_VER" cudatoolkit="$CUDA_VER" cudf=0.19 \
+        -c nvidia -c conda-forge -c defaults \
+        python="$PYTHON_VER" cudatoolkit="$CUDA_VER" \
         "$@"
 else
     conda create --yes --name "$CONDA_ENV" \
