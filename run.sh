66257e32f3813cb71f8d384479ebe63647afee57
Manolis Papadakis <mpapadakis@nvidia.com>
2021-10-25 21:19:55 -0700

    Calculate threads based on a RESERVED_CORES option

--- run.sh
+++ run.sh
@@ -40,6 +40,7 @@ if [[ $# -lt 2 || ! "$1" =~ ^(1/)?[0-9]+$ ]]; then
     echo "  NOWAIT : don't wait for batch jobs to start (default: 0)"
     echo "  PLATFORM : what machine we are executing on (default: auto-detected)"
     echo "  QUEUE : what queue/partition to submit the job to (default: depends on cluster)"
+    echo "  RESERVED_CORES : how many cores to reserve for the Legion runtime (default: 4)"
     echo "  SCRATCH : where to create an output directory (default: .)"
     echo "  TIMELIMIT : how much time to request for the job, in minutes (defaut: 60)"
     echo "  USE_CUDA : run with CUDA enabled (defaut: auto-detected)"
@@ -78,6 +79,7 @@ fi
 export MOUNTS="${MOUNTS:-}"
 export NODRIVER="${NODRIVER:-0}"
 export NOWAIT="${NOWAIT:-0}"
+export RESERVED_CORES="${RESERVED_CORES:-4}"
 export SCRATCH="${SCRATCH:-.}"
 export TIMELIMIT="${TIMELIMIT:-60}"
 export USE_CUDA="${USE_CUDA:-1}"
@@ -117,13 +119,8 @@ else
     export CMD_OUT_DIR="$HOST_OUT_DIR"
 fi
 
-# Calculate per-rank resources
-# Note that we need to set aside some CPU cores:
-# - 1 for the CPU processor
-# - 1 for the python processor
-# - 2 for the utility processors
-# - a few more for Realm worker threads and GPU processors
-# and some memory:
+# Retrieve resources available per node
+# At this point we set aside some memory:
 # - 1-2GB of framebuffer for the runtime and NCCL
 # - ~1GB of RAM for GASNet
 # - ~1GB of RAM for the Legion runtime
@@ -142,7 +139,6 @@ if [[ "$PLATFORM" == summit ]]; then
     RAM_PER_NUMA=200000
     GPUS_PER_NODE=6
     CORES_PER_NUMA=21
-    THREADS_PER_OMP=16
     FB_PER_GPU=14500
 elif [[ "$PLATFORM" == cori ]]; then
     # 2 NUMA domains per node
@@ -156,7 +152,6 @@ elif [[ "$PLATFORM" == cori ]]; then
     RAM_PER_NUMA=150000
     GPUS_PER_NODE=8
     CORES_PER_NUMA=20
-    THREADS_PER_OMP=16
     FB_PER_GPU=14500
 elif [[ "$PLATFORM" == pizdaint ]]; then
     # 1 NUMA domain per node
@@ -170,7 +165,6 @@ elif [[ "$PLATFORM" == pizdaint ]]; then
     RAM_PER_NUMA=55000
     GPUS_PER_NODE=1
     CORES_PER_NUMA=12
-    THREADS_PER_OMP=8
     FB_PER_GPU=14500
 elif [[ "$PLATFORM" == lassen ]]; then
     # 2 NUMA domains per node
@@ -184,7 +178,6 @@ elif [[ "$PLATFORM" == lassen ]]; then
     RAM_PER_NUMA=100000
     GPUS_PER_NODE=4
     CORES_PER_NUMA=20
-    THREADS_PER_OMP=16
     FB_PER_GPU=14500
 else
     echo "Did not detect a supported cluster, assuming local-node run."
@@ -203,20 +196,34 @@ else
         RAM_PER_NODE="$(free -m | head -2 | tail -1 | awk '{print $2}')"
         CORES_PER_NUMA="$(lscpu | grep 'Core(s) per socket' | awk '{print $4}')"
     fi
-    RAM_PER_NUMA=$(( RAM_PER_NODE / 2 / NUMAS_PER_NODE ))
-    THREADS_PER_OMP=$(( CORES_PER_NUMA - 4 ))
+    RAM_PER_NUMA=$(( RAM_PER_NODE / 2 / NUMAS_PER_NODE )) # use half the available system memory
     if [[ "$USE_CUDA" == 1 ]]; then
         GPUS_PER_NODE="$(nvidia-smi -q | grep 'Attached GPUs' | awk '{print $4}')"
         FB_PER_GPU=$(( $(nvidia-smi --format=csv,noheader,nounits --query-gpu=memory.total -i 0) - 2000 ))
     fi
 fi
+
+# Split available resources between ranks
+# Typically we assign all cores to OpenMP processors, except for:
+# - 1 core for the CPU processor
+# - 1 core for the python processor
+# - 2 cores for utility processors, Realm worker threads and GPU processors
 NUM_OMPS=$(( NUMAS_PER_NODE * $NODE_RATIO ))
 if [[ $NUM_OMPS -lt 1 ]]; then
     NUM_OMPS=1
-    THREADS_PER_OMP=$(( THREADS_PER_OMP * NUMAS_PER_NODE * $NODE_RATIO ))
-    RAM_PER_NUMA=$(( RAM_PER_NUMA * NUMAS_PER_NODE * $NODE_RATIO ))
+    CORES_PER_OMP=$(( CORES_PER_NUMA * NUMAS_PER_NODE * $NODE_RATIO ))
+    RAM_PER_OMP=$(( RAM_PER_NUMA * NUMAS_PER_NODE * $NODE_RATIO ))
+else
+    CORES_PER_OMP="$CORES_PER_NUMA"
+    RAM_PER_OMP="$RAM_PER_NUMA"
 fi
-NUM_CORES=$(( NUMAS_PER_NODE * CORES_PER_NUMA * $NODE_RATIO ))
+RESERVED_PER_OMP=$(( ( RESERVED_CORES + NUM_OMPS - 1 ) / NUM_OMPS ))
+if (( RESERVED_PER_OMP >= CORES_PER_OMP )); then
+    echo "Error: Not enough cores, try reducing RESERVED_CORES" 1>&2
+    exit 1
+fi
+THREADS_PER_OMP=$(( CORES_PER_OMP - RESERVED_PER_OMP ))
+NUM_CORES=$(( NUM_OMPS * CORES_PER_OMP ))
 if [[ "$USE_CUDA" == 1 ]]; then
     NUM_GPUS=$(( GPUS_PER_NODE * $NODE_RATIO ))
 fi
@@ -230,10 +237,10 @@ if [[ "$NODRIVER" != 1 ]]; then
     if [[ "$USE_OPENMP" == 1 ]]; then
         set -- --cpus 1 --sysmem 256 "$@"
         set -- --omps "$NUM_OMPS" --ompthreads "$THREADS_PER_OMP" "$@"
-        set -- --numamem "$RAM_PER_NUMA" "$@"
+        set -- --numamem "$RAM_PER_OMP" "$@"
     else
         set -- --cpus $(( NUM_OMPS * THREADS_PER_OMP )) "$@"
-        set -- --sysmem $(( NUM_OMPS * RAM_PER_NUMA )) "$@"
+        set -- --sysmem $(( NUM_OMPS * RAM_PER_OMP )) "$@"
     fi
     if [[ "$PLATFORM" == summit ]]; then
         set -- --cores-per-node $(( NUMAS_PER_NODE * CORES_PER_NUMA)) --launcher jsrun "$@"
