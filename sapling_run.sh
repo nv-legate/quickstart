0744100dca377e2d72b94d859d7a3273b938e731
Manolis Papadakis <manopapad@gmail.com>
2022-12-06 21:08:44 -0800

    Update CUDA module version on sapling

--- sapling_run.sh
+++ sapling_run.sh
@@ -27,5 +27,5 @@ unset LOADEDMODULES
 unset MODULEPATH
 unset _LMFILES_
 . /usr/share/modules/init/bash
-module load cuda/11.1 mpi/openmpi/4.1.0 slurm/20.11.4
+module load cuda/11.7 mpi/openmpi/4.1.0 slurm/20.11.4
 "$@"
